56c61eb4558261baea0606161f9c3e669a5d87ae
Anton Koksharov <anton.koksharov@spb.ruslab.org>
2022-09-15 04:51:50 +0300

    lintfix

---
+++ docs/components/dev/DisableDefaultSearchTest.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable -->
+
+<template>
+  <div>
+    <treeselect :options="options" :multiple="multiple" />
+    <label
+      ><input type="checkbox" v-model="multiple" /> Multi-select mode</label
+    >
+  </div>
+</template>
+
+<script>
+/* eslint-disable unicorn/no-abusive-eslint-disable */
+/* eslint-disable */
+import { generateOptions } from "../utils";
+
+export default {
+  data: () => ({
+    options: generateOptions(1, 500),
+  }),
+};
+</script>
